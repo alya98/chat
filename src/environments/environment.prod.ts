b3503018f22a3dbbc152b5e399440200075a4303
alya98 <alya-1998@list.ru>
2018-03-11 21:24:05 +0300

    chat

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAXNKxuH1TySDDH9632yI8aln1-H1W6Nac",
+    authDomain: "chat-e380e.firebaseapp.com",
+    databaseURL: "https://chat-e380e.firebaseio.com",
+    projectId: "chat-e380e",
+    storageBucket: "chat-e380e.appspot.com",
+    messagingSenderId: "212346429854"
+  }
+};
